e7d2c636bb0619959b4dddf4601e96a1ed07e984
AndAgio <aagiollo@gmail.com>
2022-05-15 10:25:23 +0200

    WIP - avoid ablation on pooling technique and use three runs as testing

--- train_sad.sh
+++ train_sad.sh
@@ -1,7 +1,7 @@
 # List of logs and who should be notified of issues
 topos=("small" "dfn")
 convs=("gcn" "cheb" "gin" "tag" "sg")
-pools=("mean" "sum" "max" "s2s" "att")
+pools=("mean") # "sum" "max" "s2s" "att")
 mus=(1 2 3 4 5 6 7 8)
 
 # Look for signs of trouble in each log
@@ -13,7 +13,7 @@ do
     do
       for p in ${!pools[@]};
       do
-        python train.py --model="class_${convs[$j]}_${mus[$k]}x100_${pools[$p]}" --train_scenario="existing" --train_topology="${topos[$i]}" --epochs=10 --differential=0 --masking=0
+        python train.py --model="class_${convs[$j]}_${mus[$k]}x100_${pools[$p]}" --train_scenario="existing" --train_topology="${topos[$i]}" --epochs=10 --differential=0 --masking=0 --train_sims 1 2 --val_sims 2 --test_sims 3 4 5
       done
     done
   done
